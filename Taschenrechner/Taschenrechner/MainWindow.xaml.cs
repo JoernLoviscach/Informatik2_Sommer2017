2425a3705b83efec6ef7d73b9f3c514df274364b
Jörn Loviscach <joern.loviscach@hotmail.de>
2017-04-20 15:14:20 +0200

    Anfang

---
+++ Taschenrechner/Taschenrechner/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Taschenrechner
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button0_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button3_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button4_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button5_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button6_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button7_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button8_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void Button9_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
