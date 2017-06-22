8ebe0d2e127034a977c6e831c9f041ddfea1e190
Jörn Loviscach <joern.loviscach@hotmail.de>
2017-06-22 18:44:46 +0200

    Upload nach der Veranstaltung

---
+++ ClassInterfaceStruct/ClassInterfaceStruct/MainWindow.xaml.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassInterfaceStruct
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            A a = new A();
+            A aKopie = a;
+            a.X = 42;
+            int u = aKopie.X;
+
+            B b = new B();
+            B bKopie = b;
+            b.X = 42;
+            int v = bKopie.X;
+
+            DreiDPunkt d3 = new DreiDPunkt(3.0, 4.0, 5.0);
+        }
+    }
+
+    class A : ICloneable
+    {
+        public int X;
+
+        public object Clone()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    struct B
+    {
+        public int X;
+    }
+
+    abstract class C
+    {
+        public int X;
+
+        abstract public void TuWas();
+    }
+
+    abstract class D : C
+    {
+        public int Y;
+    }
+
+    interface IKannWas
+    {
+        void TuWas();
+    }
+
+    interface IKannMehr : IKannWas
+    {
+        void TuMehr();
+    }
+
+    class E : A, IKannWas
+    {
+        public void TuWas()
+        {
+        }
+    }
+
+    struct DreiDPunkt
+    {
+        double x, y, z;
+        public double X { get { return x; } }
+        public double Y { get { return y; } }
+        public double Z { get { return z; } }
+
+        public DreiDPunkt(double x, double y, double z)
+        {
+            this.x = x;
+            this.y = y;
+            this.z = z;
+        }
+    }
+}
---
+++ Collections/Collections/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Collections
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            int[] a = new int[23];
+            List<int> b = new List<int>();
+            b.Add(13);
+            b.Add(7);
+            b.Add(4);
+            b.RemoveAt(0);
+            int x = b[0];
+            int y = b.Count;
+
+            // Warteschlange von Listen von Messwerten
+            Queue<List<Messwert>> warteschlange = new Queue<List<Messwert>>();
+
+            //List<Messwert> liste1 = new List<Messwert>();
+            //liste1.Add(new Lufttemperatur()); // Polymorphie!!!
+            //liste1.Add(new Windgeschwindigkeit());
+            //warteschlange.Enqueue(liste1);
+
+            List<Messwert> liste1
+                = new List<Messwert> { new Lufttemperatur(),
+                                       new Windgeschwindigkeit() };
+            warteschlange.Enqueue(liste1);
+
+            List<Messwert> liste2
+                = new List<Messwert> { new Windgeschwindigkeit(),
+                                       new Windrichtung(),
+                                       new Windrichtung() };
+            warteschlange.Enqueue(liste2);
+
+            liste1.RemoveAt(0);
+            Messwert m = warteschlange.Dequeue()[0];
+
+            //Stapel von Listen von Messwerten
+            Stack<List<Messwert>> stapel = new Stack<List<Messwert>>();
+            stapel.Push(liste1);
+            stapel.Push(liste2);
+            Messwert m1 = stapel.Pop()[1];
+
+            List<Queue<int>> u = new List<Queue<int>>();
+            Queue<int> q = new Queue<int>();
+            u.Add(q);
+            u.Add(q);
+            q.Enqueue(7);
+            q.Enqueue(4);
+            Queue<int> p = new Queue<int>();
+            p.Enqueue(13);
+            u.Add(p);
+            int x1 = q.Dequeue();
+            int x2 = u[1].Dequeue();
+        }
+    }
+
+    abstract class Messwert
+    {
+        DateTime zeitpunkt;
+    }
+
+    class Lufttemperatur : Messwert
+    {
+        double temperatur;
+    }
+
+    class Windgeschwindigkeit : Messwert
+    {
+        double geschwindigkeit;
+    }
+
+    class Windrichtung : Messwert
+    {
+        double richtung;
+    }
+}
